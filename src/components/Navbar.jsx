d5017f8eb92b13f2572c777b310c2e1122eaa246
Eddysam2000 <samueledwin67@gmail.com>
2023-12-13 12:07:33 +0100

    nav done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   return (
     <nav>
         <div className="container nav_container">
-            <Link to="/" className='logo'>
+            <Link to="/" className='logo' onClick={() => setIsNavShowing(false)}>
                 <img src={Logo} alt="Nav Logo" />
             </Link>
             <ul className={`nav_links ${isNavShowing ? 'show_nav' : 'hide_nav'}`}>
@@ -23,13 +23,13 @@ const Navbar = () => {
                     links.map(({name, path}, index) => {
                         return (
                             <li key={index}>
-                                <NavLink to={path} className={({isActive})=> isActive ? 'active-nav' : ''}>{name}</NavLink>
+                                <NavLink to={path} className={({isActive})=> isActive ? 'active-nav' : ''} onClick={() => setIsNavShowing(prev => !prev)}>{name}</NavLink>
                             </li>
                         )
                     })
                 }
             </ul>
-            <button className="nav_toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
+            <button className="nav_toggle-btn" onClick={() => setIsNavShowing(prev => !prev)}>
                 {
                     isNavShowing ? <MdOutlineClose/> : <FaBars/>
                 }
