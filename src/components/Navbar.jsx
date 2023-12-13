1ce5c9a8879a301ab73f8103b487e4c1391c018f
Eddysam2000 <samueledwin67@gmail.com>
2023-12-13 11:14:07 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import './navbar.css'
+import Logo from '../images/logo.png'
+import { FaBars } from 'react-icons/fa'
+import {links} from '../data'
+
+
+
+const Navbar = () => {
+    const [isNavShowing, setIsNavShowing] = useState(false);
+
+
+  return (
+    <nav>
+        <div className="container nav_container">
+            <Link to="/" className='logo'>
+                <img src={Logo} alt="Nav Logo" />
+            </Link>
+            <ul className={`nav_links ${isNavShowing ? 'show_nav' : 'hide_nav'}`}>
+                {
+                    links.map(({name, path}, index) => {
+                        return (
+                            <li>
+                                <NavLink to={path} className={({isActive})=> isActive ? 'active-nav' : ''}>{name}</NavLink>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            <button className="nav_toggle-btn">
+                <FaBars/>
+            </button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
