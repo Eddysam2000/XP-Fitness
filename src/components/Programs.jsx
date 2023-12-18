964597449d7ea97a18816a5143b204071a3d5a76
Eddysam2000 <samueledwin67@gmail.com>
2023-12-18 14:04:06 +0100

    program added

--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,11 +1,24 @@
 import { FaCrown } from 'react-icons/fa'
 import SectionHead from './SectionHead'
+import {programs} from '../data'
+import Card from '../UI/Card'
 
 const Programs = () => {
   return (
     <section className="programs">
         <div className="container programs_container">
             <SectionHead icon={<FaCrown/>} title="Programs"/>
+        </div>
+        <div className="programs_wrapper">
+            {
+                programs.map(({id, icon, title, info, path}) => {
+                    return (
+                        <Card className='programs_program' key={}>
+
+                        </Card>
+                    )
+                })
+            }
         </div>
     </section>
   )
