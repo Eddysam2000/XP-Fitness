e6dcc1933916c00aa533288118e07e8bf9f149aa
Eddysam2000 <samueledwin67@gmail.com>
2023-12-25 17:17:33 +0100

    testimonial added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router-dom"
 import Logo from '../images/logo.png'
+import { FaFacebook } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiFillInstagram } from 'react-icons/ai'
 
 const Footer = () => {
   return (
