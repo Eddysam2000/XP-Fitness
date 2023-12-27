172a5ed7499df877203454f5c52c2a6965214270
Eddysam2000 <samueledwin67@gmail.com>
2023-12-28 00:13:56 +0100

    about added

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+const Header = ({title, image, children}) => {
+  return (
+    <header className="header">
+        <div className="header_container">
+            <div className="header_container-bg">
+                
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
