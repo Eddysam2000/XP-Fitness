3c8fdd4fad138c8bd505d5f2a07963200d7d1861
Eddysam2000 <samueledwin67@gmail.com>
2023-12-29 17:15:15 +0100

    about updated

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,6 @@
 import Header from '../../components/Header'
 import HeaderImage from '../../images/header.png'
-import StoryImage from '../../images/about.png'
+import StoryImage from '../../images/hull.png'
 import './about.css'
 
 const About = () => {
@@ -13,7 +13,19 @@ const About = () => {
     <section className="about_story">
       <div className="container about_story-container">
         <div className="about_section-image">
-          <img src="" alt="" />
+          <img src={StoryImage} alt="Our Story Image" />
+        </div>
+        <div className="about_section-content">
+          <h1>Our Story</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde quis corporis amet, neque provident culpa dolorum voluptatum libero quasi voluptate aspernatur debitis numquam consequatur mollitia. Unde rerum velit blanditiis accusamus corrupti nobis et, deserunt odio dolorem id eveniet dignissimos cum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugit et rerum cupiditate dolores earum voluptates pariatur debitis porro eveniet consequatur quis corrupti, impedit quibusdam, sapiente ut? Provident, facere laudantium?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consequatur nulla, porro quidem molestias suscipit.
+          </p>
         </div>
       </div>
     </section>
