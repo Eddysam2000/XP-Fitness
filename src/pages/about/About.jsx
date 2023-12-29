b3affa289eeb763a5f05d34f4b9a56574b23fbb4
Eddysam2000 <samueledwin67@gmail.com>
2023-12-29 17:32:54 +0100

    about updated

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,6 +2,7 @@ import Header from '../../components/Header'
 import HeaderImage from '../../images/header.png'
 import StoryImage from '../../images/main.png'
 import VisionImage from '../../images/hull.png'
+import MissionImage from '../../images/mission.png'
 import './about.css'
 
 const About = () => {
@@ -33,9 +34,6 @@ const About = () => {
 
     <section className="about_vision">
       <div className="container about_vision-container">
-        <div className="about_section-image">
-          <img src={VisionImage} alt="Our Vision Image" />
-        </div>
         <div className="about_section-content">
           <h1>Our Vision</h1>
           <p>
@@ -43,6 +41,29 @@ const About = () => {
           </p>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugit et rerum cupiditate dolores earum voluptates pariatur debitis porro eveniet consequatur quis corrupti, impedit quibusdam, sapiente ut? Provident, facere laudantium?
+          </p>
+        </div>
+        <div className="about_section-image">
+          <img src={VisionImage} alt="Our Vision Image" />
+        </div>
+      </div>
+    </section>
+
+    <section className="about_mission">
+      <div className="container about_mission-container">
+        <div className="about_section-image">
+          <img src={MissionImage} alt="Our Mission Image" />
+        </div>
+        <div className="about_section-content">
+          <h1>Our Mission</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde quis corporis amet, neque provident culpa dolorum voluptatum libero quasi voluptate aspernatur debitis numquam consequatur mollitia. Unde rerum velit blanditiis accusamus corrupti nobis et, deserunt odio dolorem id eveniet dignissimos cum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugit et rerum cupiditate dolores earum voluptates pariatur debitis porro eveniet consequatur quis corrupti, impedit quibusdam, sapiente ut? Provident, facere laudantium?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consequatur nulla, porro quidem molestias suscipit.
           </p>
         </div>
       </div>
